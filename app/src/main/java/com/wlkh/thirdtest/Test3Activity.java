584a58bdd9399e4ddd4672a26056f665b42b2424
wulikanhuaa <731648638@qq.com>
2018-12-18 11:34:17 +0800

    测试新建文件提交

---
+++ app/src/main/java/com/wlkh/thirdtest/Test3Activity.java
@@ -0,0 +1,16 @@
+package com.wlkh.thirdtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 测试新建文件提交
+ */
+public class Test3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test3);
+    }
+}
